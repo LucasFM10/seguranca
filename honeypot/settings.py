0fb5bd626ec3c15a23353bda9716aea68fbf0c58
Lucas <lucasfm.py@gmail.com>
2025-04-21 15:59:32 -0300

    ok!

--- honeypot/settings.py
+++ honeypot/settings.py
@@ -116,11 +116,14 @@ USE_I18N = True
 
 USE_TZ = True
 
+import os
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
