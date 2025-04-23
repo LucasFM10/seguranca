dfafcb572cdc38e8eea06263c3e397392507fa8d
Lucas <lucasfm.py@gmail.com>
2025-04-23 09:03:13 -0300

    graficos

--- project/dcrm/settings.py
+++ project/dcrm/settings.py
@@ -20,12 +20,16 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'admin_tools_stats',
+    'django_nvd3',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'website',
     'honeypot'
 ]
--- project/dcrm/urls.py
+++ project/dcrm/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('secret-admin/', admin.site.urls),
     path('admin/', include('honeypot.urls'), name='honeypot_admin'),
     path('', include('website.urls')),
+
+    path('admin_tools_stats/', include('admin_tools_stats.urls')),
 ]
